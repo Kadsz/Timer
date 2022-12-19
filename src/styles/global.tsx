0e49c5e59734f5bb0429f4321dd74a7c7e75f37a
Carlos Eduardo <kadufenix10@gmail.com>
2022-12-19 17:03:20 -0300

    Primeiros passos da aplicação

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -5,6 +5,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    background-color: ${(props) => props.theme['gray-900']}
   }
 
   body, input, textarea, button {
